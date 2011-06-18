6103fb0d444520cc00c89e01113103e0f132eda7
Jon Gross <j.gross@f5.com>
2011-06-18 13:59:09 -0700

    remove unused function

--- application/helpers/image_helper.php
+++ application/helpers/image_helper.php
@@ -3,8 +3,5 @@
     function image_thumb_url($filename) {
         return base_url() . "images/thumbs/" . $filename;
     }
-    
-    function delete_image_url($id) {
-        return anchor('gallery/delete/' . $id, "Delete");
-    }
+
 ?>
