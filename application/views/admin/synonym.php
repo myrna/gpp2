3b31235f8c7edb7e19c1f62a2a3ada5090c49f0d
Jon Gross <j.gross@f5.com>
2011-08-02 06:55:33 -0700

    add new views

---
+++ application/views/admin/synonym.php
@@ -0,0 +1,23 @@
+<div id="content" class="admin">
+    <h2>GPP Database Administration: Add Synonym</h2>
+    <p class="nav"><?php
+       echo anchor('/listplants', 'Return to Main List');
+    ?></p>
+    <?php
+        $attributes = array('class' => 'data-entry');
+        echo form_open('crud/save_synonym', $attributes);
+        echo form_hidden('synonym_id', $synonym_id);
+    ?>
+    <?php echo $this->session->flashdata('status'); ?>
+    <ul>
+    <?php foreach ($row as $key => $value) {
+        echo "<li>";
+        echo "<span class='labelname'>";
+        echo field_to_label($key);
+        echo "</span>";
+        echo build_form_control($key, $value);
+        echo "</li>";
+    } ?>
+    </ul>
+    <?php echo form_submit('save_synonym', 'Add Synonym'); ?>
+</div>
